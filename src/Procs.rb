9296d7e80690cc8cf3da0e641383203bb905c6fa
Alessio Saltarin <alessiosaltarin@gmail.com>
2014-11-04 17:55:25 +0100

    Procs

--- src/Procs.rb
+++ src/Procs.rb
@@ -1,3 +1,22 @@
 
 
-procedura_uno = Proc.new 
+procedura_uno = Proc.new  do
+    puts "Questo e' un blocco"
+    puts "composto da due comandi"
+end
+
+procedura_due = Proc.new do |x|
+  puts "Hai passato il valore " + x.to_s
+end
+
+procedura_uno.call
+procedura_due.call 2
+
+def miaFunzione(procedura)  
+  puts "ESEGUO LA PROCEDURA:"
+  procedura.call
+end
+
+miaFunzione procedura_uno
+
+
